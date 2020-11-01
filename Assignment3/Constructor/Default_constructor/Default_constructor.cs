001809496cd5249529ce6a3fa40c0888ec31e547
Rachit Bucha <rachit.rj11@gmail.com>
2020-11-01 20:56:59 +0530

    Default_Constructor

---
+++ Assignment3/Constructor/Default_constructor/Default_constructor.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Default_constructor
+{ 
+class Rachit
+{
+
+    int num;
+    string name;
+ 
+    Rachit()
+    {
+        Console.WriteLine("Constructor Called");
+    }
+
+    public static void Main()
+    {
+ 
+        Rachit Rachit1 = new Rachit();
+
+        Console.WriteLine(Rachit1.name);
+        Console.WriteLine(Rachit1.num);
+    }
+}
+}
