8dd012e18edfa1a5a5a487895c6885fd8470b7f0
Rachit Bucha <rachit.rj11@gmail.com>
2020-11-01 20:16:44 +0530

    internal_Access_Modifer

---
+++ Assignment3/internalaccessmodifier/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace internalAccessModifier
+{
+
+     
+    internal class Complex
+    {
+
+        int real;
+        int img;
+
+        public void setData(int r, int i)
+        {
+            real = r;
+            img = i;
+        }
+
+        public void displayData()
+        {
+            Console.WriteLine("Real = {0}", real);
+            Console.WriteLine("Imaginary = {0}", img);
+        }
+    }
+
+   
+    class Program
+    {
+
+  
+        static void Main(string[] args)
+        {
+            
+            Complex c = new Complex();
+
+            
+            c.setData(2, 1);
+            c.displayData();
+        }
+    }
+}
