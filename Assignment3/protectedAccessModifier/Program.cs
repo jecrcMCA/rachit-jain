da2f46e5fcbc26ff27f8804cab992829322104a0
Rachit Bucha <rachit.rj11@gmail.com>
2020-11-01 20:11:39 +0530

    Protected_Access_Modifer

---
+++ Assignment3/protectedAccessModifier/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace protectedAccessModifier
+{
+
+    class X
+    {
+        protected int x;
+
+        public X()
+        {
+            x = 10;
+        }
+    }
+
+    class Y : X
+    {
+
+
+        public int getX()
+        {
+            return x;
+        }
+    }
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            X obj1 = new X();
+            Y obj2 = new Y();
+
+
+            Console.WriteLine("Score of Rachit is : {0}", obj2.getX());
+        }
+    }
+}
