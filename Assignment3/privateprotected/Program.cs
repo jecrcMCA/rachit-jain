98a7a0666628ab01a5601839ed6e039e3bbf0e1d
Rachit Bucha <rachit.rj11@gmail.com>
2020-11-01 20:48:40 +0530

    private_protected_Access_Modifer

---
+++ Assignment3/privateprotected/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace PrivateProtectedAccessModifier
+{
+
+	class Parent
+	{
+
+		
+		private protected int value;
+
+		
+		public void setValue(int v)
+		{
+			value = v;
+		}
+		public int getValue()
+		{
+			return value;
+		}
+	}
+
+	class Child : Parent
+	{
+
+		public void showValue()
+		{
+
+			Console.WriteLine("Value = " + value);
+		}
+	}
+
+	class Program
+	{
+
+		static void Main(string[] args)
+		{
+			Parent obj = new Parent();
+
+			obj.setValue(4);
+			Console.WriteLine("Value = " + obj.getValue());
+		}
+	}
+}
