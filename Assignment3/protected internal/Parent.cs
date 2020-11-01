f1cde024f95aaa1f641279816ecee4f8e0e8334b
Rachit Bucha <rachit.rj11@gmail.com>
2020-11-01 20:40:05 +0530

    protected_internal_Access_Modifer

---
+++ Assignment3/protected internal/Parent.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public class Parent
+{
+
+	protected internal int value;
+}
+
+class ABC
+{
+
+	public void testAccess()
+	{
+		Parent obj1 = new Parent();
+		obj1.value = 12;
+	}
+}
---
+++ Assignment3/protected internal/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Program
+{
+
+	class Child : Parent
+	{
+
+		// Main Method 
+		public static void Main(String[] args)
+		{
+			Child obj3 = new Child();
+
+			obj3.value = 9;
+			Console.WriteLine("Value = " + obj3.value);
+		}
+	}
+}
